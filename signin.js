9353b0fbdd71fd9865476d77a24a25a0aaa1f9ca
Dhanada Panda <130206432+Dhanada-Panda@users.noreply.github.com>
2023-08-09 15:49:21 +0530

    signin.js updated

--- signin.js
+++ signin.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const name = document.getElementById("name");
-  const blood = document.getElementById("blood");
-  const button = document.getElementById("signin");
+  const name = document.getElementById("username");
+  const password = document.getElementById("password");
+  const button = document.getElementById("signin-button");
 
   button.addEventListener("click", () => {
     fetch("http://localhost:3000/donors", {
@@ -10,14 +10,14 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((res) => res.json())
       .then((data) => {
         const comparison = data.find(
-          (ele) => ele.name === name.value && ele.blood === blood.value
+          (ele) => ele.name === name.value && ele.password === password.value
         );
         console.log(comparison);
         if (comparison !== undefined) {
           alert("Logged in successfully");
           localStorage.setItem("token", JSON.stringify(Date.now()));
           localStorage.setItem("id", JSON.stringify(comparison.id));
-          window.location.href = "index.html";
+          window.location.href = "info.html";
         } else {
           alert("Incorrect input");
         }
